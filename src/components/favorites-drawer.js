97595e5eebcf93004d4513211b54fa0d5b387b07
Bulat Khabibullin <bulat.kt@yandex.ru>
2022-05-25 18:25:22 +0300

    feat(favorites): add star icon

--- src/components/favorites-drawer.js
+++ src/components/favorites-drawer.js
@@ -9,8 +9,14 @@ import {
   DrawerContent,
   DrawerCloseButton,
   Flex,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Tooltip
 } from "@chakra-ui/core";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
+import { Star } from "react-feather";
 
 
 export default function FavoritesDrawer({ children }) {
@@ -24,15 +30,17 @@ export default function FavoritesDrawer({ children }) {
 
   return (
     <Flex flexDirection='column'>
-      <Button
-        ref={btnRef}
-        colorScheme='teal'
-        onClick={onDrawerOpen}
-        roundedTop="0"
-        alignSelf={'flex-end'}
-      >
-        Favorites
-      </Button>
+      <Tooltip label="See favorites" hasArrow>
+        <Button
+          ref={btnRef}
+          colorScheme='teal'
+          onClick={onDrawerOpen}
+          roundedTop="0"
+          alignSelf={'flex-end'}
+        >
+          <Star />
+        </Button>
+      </Tooltip>
       {children}
       <Drawer
         isOpen={isOpen}
@@ -44,14 +52,12 @@ export default function FavoritesDrawer({ children }) {
         <DrawerContent overflow='scroll'>
           <DrawerCloseButton />
           <DrawerHeader>Favorites</DrawerHeader>
-
           <DrawerBody>
             <Tabs>
               <TabList>
                 <Tab>Launches</Tab>
                 <Tab>Launch Pads</Tab>
               </TabList>
-
               <TabPanels>
                 <TabPanel>
                   <div style={{ border: '1px solid red', height: 200 }}>hello 1</div>
