bf90117b292a167759b7d72d7f305270895799c4
Bulat Khabibullin <bulat.kt@yandex.ru>
2022-05-26 22:15:39 +0300

    feat(favorites): persist recoil state

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,11 +1,15 @@
 import { atom } from "recoil";
+import { recoilPersist } from 'recoil-persist';
+const { persistAtom } = recoilPersist();
 
 export const favLaunchesState = atom({
   key: 'favLaunchesState',
-  default: []
+  default: [],
+  effects_UNSTABLE: [persistAtom]
 });
 
 export const favLaunchPadsState = atom({
   key: 'favLaunchPadsState',
-  default: []
+  default: [],
+  effects_UNSTABLE: [persistAtom]
 });
