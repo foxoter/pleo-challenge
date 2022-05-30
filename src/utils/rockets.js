e47f505386744b7d27b2df11f288bd669f1af78a
Bulat Khabibullin <bulat.kt@yandex.ru>
2022-05-30 15:22:48 +0300

    fix(task-3): refactor rockets data sorting

--- src/utils/rockets.js
+++ src/utils/rockets.js
@@ -1,35 +1,18 @@
-import { SORT_BY_KEYS } from "../constants/rockets";
+import { SORT_BY_KEYS, ROCKET_DATA_PATHS } from "../constants/rockets";
 import { getMiliseconds } from "./format-date";
+import { get } from "lodash";
 
 export function sortRocketsData(array, sortBy, sortOrder) {
-  switch (sortBy) {
-    case SORT_BY_KEYS.cost:
-      sortOrder === "asc"
-        ? array.sort((a, b) => a.cost_per_launch - b.cost_per_launch)
-        : array.sort((a, b) => b.cost_per_launch - a.cost_per_launch);
-      return array;
-    case SORT_BY_KEYS.height:
-      sortOrder === "asc"
-        ? array.sort((a, b) => a.height.meters - b.height.meters)
-        : array.sort((a, b) => b.height.meters - a.height.meters);
-      return array;
-    case SORT_BY_KEYS.mass:
-      sortOrder === "asc"
-        ? array.sort((a, b) => a.mass.kg - b.mass.kg)
-        : array.sort((a, b) => b.mass.kg - a.mass.kg);
-      return array;
-    case SORT_BY_KEYS.date:
-      sortOrder === "asc"
-        ? array.sort(
-            (a, b) =>
-              getMiliseconds(a.first_flight) - getMiliseconds(b.first_flight),
-          )
-        : array.sort(
-            (a, b) =>
-              getMiliseconds(b.first_flight) - getMiliseconds(a.first_flight),
-          );
-      return array;
-    default:
-      return array;
+  sortOrder === "asc"
+    ? array.sort((a, b) => getRocketData(a, sortBy) - getRocketData(b, sortBy))
+    : array.sort((a, b) => getRocketData(b, sortBy) - getRocketData(a, sortBy));
+
+  return array;
+}
+
+export function getRocketData(obj, key) {
+  if (key === SORT_BY_KEYS.date) {
+    return getMiliseconds(get(obj, ROCKET_DATA_PATHS[key]));
   }
+  return get(obj, ROCKET_DATA_PATHS[key]);
 }
